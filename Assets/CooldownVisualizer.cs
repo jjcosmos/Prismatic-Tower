3df386185b8097caad73a32ad2da5ea667d74a0a
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-09-06 13:56:52 -0700

    cooldownsIn

---
+++ Assets/CooldownVisualizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class CooldownVisualizer : MonoBehaviour
+{
+    [SerializeField] List<Image> abilityArray;
+    WaitForSecondsRealtime increment;
+    IEnumerator Start()
+    {
+        increment = new WaitForSecondsRealtime(.1f);
+        yield return null;
+        AttackInput playerAttackInput = PersistantPlayer.StaticInstance.playerAttackInput;
+        while (true)
+        {
+            int i = 0;
+            foreach (Attackdef attack in playerAttackInput.attacks)
+            {
+                Color tempColor = abilityArray[i].color;
+                tempColor.a = attack.currentCooldown / attack.attackCooldown;
+                tempColor.a *= Convert.ToInt32(attack.isUnlocked);
+                //Debug.Log($"{attack.lensType} alpha is {tempColor.a}");
+                abilityArray[i].color = tempColor;
+                i++;
+            }
+            yield return increment;
+        }
+    }
+
+    
+}
